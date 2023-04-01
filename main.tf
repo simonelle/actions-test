f13b079e7f3a508cd5f8cfe90f076f13cdf50193
simonelle <97528755+simonelle@users.noreply.github.com>
2023-04-01 17:00:22 -0400

    release features and connditions included

--- main.tf
+++ main.tf
@@ -6,5 +6,26 @@ resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
         Name = "class30"
+        Teams = "DevOps"
+        Environment = "dev"
+        Division = "HR"
     }  
+}
+
+terraform {
+    required_version = "~> 1.0"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            versioversion = "~> 3.0"  #optional but recommended in productions
+                    
+      
+    }
+}
+backend "s3" {
+    bucket = "myclass30bucket"
+    key = "prod/terraform.tfstate"
+    region = "us-west-2"
+  
+}
 }
