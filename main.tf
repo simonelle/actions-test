20efcec204fbbf8ed1fa7573d91ef8f12bf7ecb5
simonelle <97528755+simonelle@users.noreply.github.com>
2023-04-03 18:45:24 -0400

    editted first.yaml file

--- main.tf
+++ main.tf
@@ -4,28 +4,11 @@ provider "aws" {
 }
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
+
     tags = {
         Name = "class30"
         Teams = "DevOps"
         Environment = "dev"
         Division = "HR"
     }  
-}
-
-terraform {
-    required_version = "~> 1.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            versioversion = "~> 3.0"  #optional but recommended in productions
-                    
-      
-    }
-}
-backend "s3" {
-    bucket = "myclass30bucket"
-    key = "prod/terraform.tfstate"
-    region = "us-east-2"
-  
-}
 }
