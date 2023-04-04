744453f1f9a22717825be92106e3054fe4877a4a
simonelle <97528755+simonelle@users.noreply.github.com>
2023-04-04 10:06:39 -0400

    fix: semantic versioning

--- release.config.js
+++ release.config.js
@@ -1,9 +1,10 @@
-module.exports = {
+ module.exports = {
     branches: "main",
-    repositoryUrl: "https://github.com/simonelle/actions-test.git",
+    repositoryUrl: "https://github.com/simonelle/action-test.git",
     plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/git',
       '@semantic-release/github']
  }
+
